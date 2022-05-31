743c64c81081cdc4ce65884c8c90315d75cd9459
David Kosorin <david@kosorin.net>
2022-05-31 09:36:37 +0200

    Split numbers on number row by odd and even

--- src/keymaps/default/keymap.c
+++ src/keymaps/default/keymap.c
@@ -32,7 +32,7 @@ void eeconfig_init_user(void) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_BASE] = LAYOUT(
     /*                                                                                                                                                                                            */
-    /**/    KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                   /**/                KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_GRV,         /**/
+    /**/    KC_ESC,     KC_7,       KC_5,       KC_3,       KC_1,       KC_9,                   /**/                KC_8,       KC_0,       KC_2,       KC_4,       KC_6,       KC_GRV,         /**/
     /**/    KC_TAB,     KC_Q,       KC_W,       KC_F,       KC_D,       KC_G,                   /**/                KC_J,       KC_L,       KC_U,       KC_Y,       KC_QUOT,    KC_MINS,        /**/
     /**/    KC_LSFT,    KC_A,       KC_R,       KC_S,       KC_T,       KC_P,       KC_MUTE,    /**/    K_ENC_SM,   KC_M,       KC_N,       KC_E,       KC_I,       KC_O,       KC_RSFT,        /**/
     /**/    KC_LCTL,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                   /**/                KC_K,       KC_H,       KC_COMM,    KC_DOT,     KC_QUES,    KC_RCTL,        /**/
