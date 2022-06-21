d1235fbfbdb2913756422a79fcde5c0718fa5982
David Kosorin <david@kosorin.net>
2022-06-21 22:14:12 +0200

    Add progress info images

--- src/keymaps/default/oled.km.c
+++ src/keymaps/default/oled.km.c
@@ -90,7 +90,7 @@ static void render_layers(void) {
     uint8_t layer = get_highest_layer(layer_state);
     for (uint8_t row = 0; row < sprite_height; row++) {
         oled_set_cursor(column_offset, line_offset + row);
-        oled_write_raw_P(layer_sprites + ((sprite_width * row) + (layer * sprite_width * sprite_height)), sprite_width);
+        oled_write_raw_P(layer_sprites + (layer * sprite_width * sprite_height) + (sprite_width * row), sprite_width);
     }
 }
 
@@ -133,56 +133,87 @@ static void render_caps_lock(void) {
     }
 }
 
-static void render_progress_ring(void) {
+static void render_progress(void) {
     static const uint8_t column_offset = 0;
     static const uint8_t line_offset = 0;
-    static const uint8_t fps = 10;
-    static const uint8_t frame_count = 8;
+    static const uint8_t ring_fps = 10;
+    static const uint8_t ring_frame_count = 8;
     static const uint8_t sprite_height = SPRITE_HEIGHT(2);
     static const uint8_t sprite_width = SPRITE_WIDTH(2);
-    static const char empty_sprite[SPRITE_HEIGHT(2) * SPRITE_WIDTH(2)] PROGMEM = 
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
-    static const char ring_sprites[][SPRITE_HEIGHT(2) * SPRITE_WIDTH(2)] PROGMEM = {
-        { 0, 0, 0, 2, 14, 31, 15, 15, 15, 31, 14, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 12, 31, 62, 126, 124, 56, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, 240, 240, 248, 224, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 7, 7, 15, 3, 0, },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24, 124, 62, 63, 31, 14, 2, 0, },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, 56, 124, 120, 120, 120, 124, 56, 32, 0, 0, 0, 0, },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 14, 31, 63, 62, 124, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-        { 224, 248, 240, 240, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 15, 7, 7, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-        { 32, 56, 124, 126, 62, 31, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+    static const char empty_sprite[] PROGMEM = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    };
+    static const char ring_sprites[] PROGMEM = {
+        0, 0, 0, 2, 14, 31, 15, 15, 15, 31, 14, 2, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 12, 31, 62, 126, 124, 56, 32, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, 240, 240, 248, 224, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 7, 7, 15, 3, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 24, 124, 62, 63, 31, 14, 2, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 32, 56, 124, 120, 120, 120, 124, 56, 32, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        2, 14, 31, 63, 62, 124, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        224, 248, 240, 240, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        3, 15, 7, 7, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        32, 56, 124, 126, 62, 31, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     };
 
+    const char *info_sprite = empty_sprite;
     bool enabled = false;
     if (false) { }
 #if defined(KEY_LOCK_ENABLE)
     else if (watching) {
+        static const char key_lock_sprite[] PROGMEM = {
+            0, 0, 0, 42, 62, 28, 28, 254, 254, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 60, 126, 102, 102, 102, 103, 103, 102, 102, 102, 126, 60, 0, 0,
+        };
+        info_sprite = key_lock_sprite;
         enabled = true;
     }
 #endif
 #if defined(KEY_CHAIN_ENABLE)
     else if (is_key_chain_active()) {
+        static const char key_chain_sprite[] PROGMEM = {
+            56, 184, 216, 240, 224, 224, 224, 224, 224, 224, 240, 216, 184, 56, 56, 56,
+            28, 29, 29, 14, 6, 8, 12, 12, 0, 6, 14, 29, 29, 28, 28, 28,
+        };
+        info_sprite = key_chain_sprite;
         enabled = true;
     }
 #endif
 #if defined(DYNAMIC_MACRO_ENABLE)
     else if (is_dynamic_macro_recording()) {
+        static const char dynamic_macro_sprite[] PROGMEM = {
+            251, 250, 11, 251, 11, 251, 11, 251, 7, 255, 255, 7, 199, 199, 199, 199,
+            223, 95, 208, 223, 208, 223, 208, 223, 224, 255, 255, 224, 239, 235, 235, 239,
+        };
+        info_sprite = dynamic_macro_sprite;
         enabled = true;
     }
 #endif
 
-    SPRITE_ANIMATION_TICK(frame, fps, frame_count, enabled);
+    SPRITE_ANIMATION_TICK(ring_frame, ring_fps, ring_frame_count, enabled);
 
     for (uint8_t row = 0; row < sprite_height; row++) {
         oled_set_cursor(column_offset, line_offset + row);
-        oled_write_raw_P((enabled ? ring_sprites[frame] : empty_sprite) + (sprite_width * row), sprite_width);
+        oled_write_raw_P((enabled ? ring_sprites + (ring_frame * sprite_width * sprite_height) : empty_sprite) + (sprite_width * row), sprite_width);
+    }
+
+    for (uint8_t row = 0; row < sprite_height; row++) {
+        oled_set_cursor(column_offset, line_offset + sprite_height + row);
+        oled_write_raw_P(info_sprite + (sprite_width * row), sprite_width);
     }
 }
 
 static bool oled_task_user_master(void) {
     render_layers();
     render_caps_lock();
-    render_progress_ring();
+    render_progress();
     return PROCESS_HANDLED;
 }
 
