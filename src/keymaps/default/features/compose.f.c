d0fa3c4082e736496b074120712c319624e4eb63
David Kosorin <david@kosorin.net>
2022-08-26 03:17:02 +0200

    Fix composed uppercase letters

--- src/keymaps/default/features/compose.f.c
+++ src/keymaps/default/features/compose.f.c
@@ -13,16 +13,16 @@ bool process_compose(uint16_t keycode, const keyrecord_t *record) {
     return PROCESS_NOT_HANDLED;
 }
 
-static uint8_t get_and_clear_all_mods(uint16_t keycode) {
+static uint8_t get_and_clear_all_mods(uint8_t compose_index) {
     uint8_t mods = get_mods() | get_oneshot_mods() | get_weak_mods();
-    
+
     clear_mods();
     clear_oneshot_mods();
     clear_weak_mods();
-        
+
 #if defined(CAPS_WORD_ENABLE)
     if (is_caps_word_on()) {
-        if (caps_word_press_user(keycode)) {
+        if (caps_word_press_user(COMPOSE(compose_index))) {
             mods |= get_weak_mods();
             clear_weak_mods();
         }
@@ -39,23 +39,24 @@ void process_compose_direct(uint8_t compose_index) {
     if (compose_index >= XC__COUNT) {
         return;
     }
-    
+
     const char **compose_data = compose_map[compose_index];
     if (compose_data == NULL || compose_data[0] == NULL) {
         return;
     }
 
-    const bool saved_caps_lock = host_keyboard_led_state().caps_lock;
     const uint8_t saved_mods = get_mods();
-    
+    const bool saved_caps_lock = host_keyboard_led_state().caps_lock;
+
     if (saved_caps_lock) {
         tap_code(KC_CAPS_LOCK);
     }
-        
-    const uint8_t mods = get_and_clear_all_mods(COMPOSE(compose_index));
-    const bool shifted = compose_data[1] != NULL && ((mods & MOD_MASK_SHIFT) ^ saved_caps_lock);
-    const char *compose_string = compose_data[shifted ? 1 : 0];
-        
+
+    const uint8_t mods = get_and_clear_all_mods(compose_index);
+    const bool shifted = (mods & MOD_MASK_SHIFT) ? 1 : 0;
+    const bool uppercase = compose_data[1] != NULL && (shifted ^ saved_caps_lock);
+    const char *compose_string = compose_data[uppercase ? 1 : 0];
+
     tap_code(COMPOSE_KEY);
     send_string(compose_string);
 
@@ -63,6 +64,6 @@ void process_compose_direct(uint8_t compose_index) {
     if (saved_caps_lock) {
         tap_code(KC_CAPS_LOCK);
     }
-    
+
     return;
 }
