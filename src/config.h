cfdc1d7fda4bc54262bfd7f1d307e42a98c4e01b
David Kosorin <david@kosorin.net>
2022-09-09 02:39:32 +0200

    Remove deprecated macro `UNUSED_PINS`

--- src/config.h
+++ src/config.h
@@ -2,12 +2,17 @@
 
 #include "config_common.h"
 
+// Unused pins:
+// - D0 (SCL) - used for SOFT_SERIAL_PIN
+// - D1 (SDA)
+// - D2
+// - D3
+
 // Matrix
 #define MATRIX_ROWS (6*2)
 #define MATRIX_COLS (6)
 #define MATRIX_COL_PINS { B5, B4, E6, D7, C6, D4 }
 #define MATRIX_ROW_PINS { B6, B2, B3, B1, F7, F6 }
-#define UNUSED_PINS { D2, D3 }
 #define DIODE_DIRECTION COL2ROW
 
 // Rotary encoder
@@ -19,7 +24,6 @@
 #define ENCODER_RESOLUTIONS_RIGHT { 2 }
 
 // Communication
-// #define USE_I2C // SCL=D0 SDA=D1
 #define SOFT_SERIAL_PIN D0
 
 // Handedness
