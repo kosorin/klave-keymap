af744f62df71c5935f65f7acbbb53f01aa789ea0
David Kosorin <david@kosorin.net>
2024-07-14 22:42:59 +0200

    Rename functions

--- src/promicro_led.c
+++ src/promicro_led.c
@@ -22,11 +22,11 @@ void led_blink(pin_t pin, uint16_t delay, uint8_t count) {
     gpio_write_pin_high(pin);
 }
 
-void led_blink_main(uint16_t delay, uint8_t count) {
+void led_blink_single(uint16_t delay, uint8_t count) {
     led_blink(B0, delay, count);
 }
 
-void led_blink_both(uint16_t delay, uint8_t count) {
+void led_blink_alternate(uint16_t delay, uint8_t count) {
     for (uint8_t i = 0; i < count; i++) {
         led_blink(B0, delay, 1);
         led_blink(D5, delay, 1);
@@ -34,18 +34,18 @@ void led_blink_both(uint16_t delay, uint8_t count) {
 }
 
 void led_blink_start(void) {
-    led_blink_main(50, 2);
+    led_blink_single(50, 2);
 }
 
 void led_blink_end(void) {
-    led_blink_main(150, 2);
+    led_blink_single(150, 2);
 }
 
 void led_blink_cancel(void) {
-    led_blink_both(50, 2);
-    led_blink_main(300, 1);
+    led_blink_alternate(50, 2);
+    led_blink_single(300, 1);
 }
 
 void led_blink_error(void) {
-    led_blink_both(50, 2);
+    led_blink_alternate(50, 2);
 }
--- src/promicro_led.h
+++ src/promicro_led.h
@@ -8,8 +8,8 @@
 void led_init(void);
 
 void led_blink(uint8_t pin, uint16_t delay, uint8_t count);
-void led_blink_main(uint16_t delay, uint8_t count);
-void led_blink_both(uint16_t delay, uint8_t count);
+void led_blink_single(uint16_t delay, uint8_t count);
+void led_blink_alternate(uint16_t delay, uint8_t count);
 
 void led_blink_start(void);
 void led_blink_end(void);
